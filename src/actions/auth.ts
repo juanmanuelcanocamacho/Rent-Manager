d9fee330834bcb01399dd0786c6367b4bb3b848d
Juan Manuel Cano Camacho <juanmanuelcanocamacho@MacBook-Pro-de-Juan.local>
2026-02-09 19:02:45 +0100

    fix(auth): redirect to dashboard on login to avoid double redirect

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -8,7 +8,7 @@ export async function login(prevState: any, formData: FormData) {
         console.log('[Action] Login action started');
         await signIn('credentials', {
             ...Object.fromEntries(formData),
-            redirectTo: '/me',
+            redirectTo: '/dashboard',
         });
     } catch (error) {
         console.log('[Action] Login action caught error:', error);
